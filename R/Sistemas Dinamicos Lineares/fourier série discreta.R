3023e97e9321c28d00c060a461c0d26f4ec1eaca
mathspiritual <boralaemcasa@gmail.com>
2021-11-24 10:32:04 -0300

    Já fiz os testes 10 e 11. Aqui está o enunciado da lista 2.

--- R/Sistemas Dinamicos Lineares/fourier série discreta.R
+++ R/Sistemas Dinamicos Lineares/fourier série discreta.R
@@ -3,39 +3,99 @@ gc()
 
 source("V:\\sem backup\\_4 anal sist dinamicos lineares\\lista 1\\util.R")
 
-##########
+########## SÉRIE DE FOURIER DISCRETA
 
-os coeficientes da representacao em serie de Fourier dos seguintes sinais:
+Reduce[exp(i*pi*x) = -1, Integers]
 
-x(t) = sum k = -infty to infty, a(k) exp(j * k * omega0 * t)
+##########################
 
-a(k) = 1/T0 * integral(0, T0, x(t) * exp(-j*k*omega0*t) dt)
+N = T0 = 2*pi/omega0
 
-sinc(u) = sin(pi*u)/pi/u
-
-# no wolfram alpha, sinc(x) = sin(x)/x
-
-N = T = 2*pi/omega0
-
-sum 1 .. N: a(k) exp(j * k * omega0 * n)
+x(n) = sum k = 1 to N, a(k) exp(j * k * omega0 * n)
 
 ou translada de k .. k + N
 com a(k) = a(k + N)
 
-a(k) = 1/T0 * sum(1, N, x[n] * exp(-j*k*omega0*n) )
+# a(k) = 1/T0 * sum n=1 to T0, x[n] * exp(-i*k*w*n)
+
+##########################
+
+Linearidade
+
+1) z(n) = A x(n) + B y(n)
+   c_k = A a_k + B b_k
+
+Deslocamento no tempo
+   
+2) y(n) = x(n - n0)
+   b_k = exp(- j * k * omega0 * n0) * a_k
+   
+Deslocamento na frequência
+
+3) y(n) = exp(j*M*omega0*n)
+   b_k = a_[k - M] 
+
+Conjugação
+
+4) y(n) = conjugate(x(n))
+   b_k = conjugate(a(-k))
+
+Reflexão no tempo
+
+5) y(n) = x(-n)
+   b_k = a(-k)
 
 Escalonamento
 
-x1 = x(alpha * t) = sum k = -infty to infty, a(k) exp(j * k * (alpha * omega0) * t)
-omega1 = alpha * omega0
-T1 = 2*pi/omega1 = 2*pi/alpha/omega0 = T0/alpha
-a(k) é o mesmo
+6) y(n) = x(n/m), se n for múltiplo de m > 0
+   y(n) = 0,      se n não for múltiplo de m
+   b_k = 1/m * a_k
+   omega1 = omega0/m
+   T1 = 2*pi/omega1 = 2*pi/omega0*m = T0 * m
 
-# a(k) = 1/T0 * integrate x(t) * exp(-i*k*w*t) dt from 0 to T0
+Convolução
 
-# a(k) = 1/N * sum x[n] * exp(-i*k*w*n), n=1 to N
+7) z(t) = x(n) ** y(n) = sum k=-infty to infty, x[k] y[n - k]
+   c_k = T0 * a_k b_k
 
-DISCRETO
+Multiplicação / Modulação
+
+8) z(n) = x(n) y(n)
+   c_k = a_k ** b_k = sum L=1 to T0, a[L] b[k - L]
+
+Diferença no tempo
+
+9) y(n) = x(n) - x(n - 1)
+   b_k = (1 - exp(-j*k*omega0)) * a_k
+
+Diferenciação na frequência
+
+10) \emptyset
+
+Soma
+
+11) y(n) = sum k = -infty to n, x(k)
+    b_k = 1/(1 - exp(-j*k*omega0)) * a_k
+
+Sinais reais e pares
+	
+12) x(n) real e par
+    a_k real e par
+
+Sinais reais e ímpares
+
+13) x(n) real e ímpar
+    a_k puramente imaginário e ímpar
+
+Relações de Parseval
+
+14) 1/T0 * sum n = 1 to T0, abs(x(n))^2 = sum k = 1 to T0, abs(a(k))^2
+
+Dualidade
+
+15) \emptyset
+   
+##########################
 
 1)
 
@@ -45,9 +105,9 @@ N = 21
 
 x(n) = sum 1 .. 21: a(k) exp(j * k * 2*pi/21 * n)
 
-# a(k) = 1/21 * sum (sin(4*pi/21*n) + cos(10*pi/21*n)) * exp(-i*k*2*pi/21*n), n=1 to 21
+# a(k) = 1/21 * sum n=1 to 21, (sin(4*pi/21*n) + cos(10*pi/21*n)) * exp(-i*k*2*pi/21*n)
 
-#########
+##########################
 
 a <- function(k, T0, x) {
    soma <- 0
@@ -74,11 +134,11 @@ for (k in -10:10)
 for (n in -10:10)
    print(paste(n, " ", x(n) - recoverx(n, 21, x)))
 
-######
+##########################
 
 2)
 
-x(n) = sum 1 .. N: (1 + cos(pi/2*k)) exp(j * k * w * n)
+x(n) = sum k = 1 to T0, (1 + cos(pi/2*k)) exp(j * k * w * n)
 
 f <- function(k) {
    return (1 + cos(pi/2*k))
@@ -94,11 +154,13 @@ x <- function(n) {
 dev.off()
 grafico_discreto(x, -10, 10, -2, 5, 'blue')
 
-######
+# Reduce[e^((i pi n)/2) + e^((3 i pi n)/2) + 2 e^(2 i pi n) == 4*cos(pi/4*n)^2, n, Integers]
+
+##########################
 
 3)
 
-x(n) = sum 1 .. 24: a(k) exp(j * k * w * n)
+x(n) = sum k = 1 to 24, a(k) exp(j * k * w * n)
 
 x <- function(n) {
    return(1 + sin(pi/12 * n + 3*pi/8))
@@ -114,18 +176,18 @@ for (n in -10:10)
 print(a(1,24,x) - exp(j*3*pi/8)/2/j)
 print(a(-1,24,x) + exp(-j*3*pi/8)/2/j)
 
-######
+##########################
 
 4)
 
 x(n) = - exp(-i*3*pi/8)/2/i* exp(-i * 2*pi/24 * n) + 1 + exp(i*3*pi/8)/2/i * exp(i * 2*pi/24 * n)
      = cos(pi/8 - (pi n)/12) + 1
 
-######
+##########################
 
 2)
 
-x(n) = sum 4 .. 12: a(k) exp(j * k * 2 * pi/9 * n)
+x(n) = sum k = 4 to 12, a(k) exp(j * k * 2 * pi/9 * n)
 
 mod a_k = de -5 a 3, 4 a 12
 a_4 = 0 
@@ -154,7 +216,7 @@ y <- function(n) {
 for (n in -10:10)
    print(paste(n, " ", x(n)))
    
-####
+##########################
 
 x <- function(n) {
    n <- n %% 4
@@ -174,3 +236,21 @@ for (k in -10:10)
 for (n in -10:10)
    print(paste(n, " ", x(n) - recoverx(n, 24, x)))
 
+##########################
+
+1a) x(n) = sin(2 pi n/7 + pi/6) + exp(i pi n/5)
+
+1b) x(n) = cos^2(pi n/5)
+
+1c) x(n) = sum k = -infty to infty, p(n - k*C)
+    p(n) = - delta(n + 1) + 2 delta(n) - delta(n - 1)
+	C > 2
+	
+1d) x(n) = sum k = -infty to infty, q(n - k*3)
+    q(n) = p(n) - p(-n)
+	p(n) = 2 delta(n + 1) - delta(n) + delta(n - 1)
+	
+4) result = abs(x(0))^2 + abs(x(1))^2 + abs(x(2))^2 
+   T0 = 3
+   a_k = 1 + cos(k * 2 pi/3)
+	 
