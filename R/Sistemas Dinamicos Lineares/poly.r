05b6d9978a17ea2abb170de1e440b295d718bd94
mathspiritual <boralaemcasa@gmail.com>
2022-01-07 20:32:23 -0300

    https://github.com/BIMK/PlatEMO Disseram que eu estava reinventando essa roda aí. Como é que o artigo é de 2020?

--- R/Sistemas Dinamicos Lineares/poly.r
+++ R/Sistemas Dinamicos Lineares/poly.r
@@ -1,6 +1,6 @@
 library('polynom')
 p <- as.polynomial(c(0,7.4,1.8))
-q <- as.polynomial(c(1,1/0.860716)) 
+q <- as.polynomial(c(1,1/0.860716))
 r <- as.polynomial(c(0.414937,0.389284,1))
 a <- p/q/r
 p <- p %% (q*r)
@@ -56,10 +56,40 @@ polysum <- function(a, b) {
 }
 
 j <- complex(imaginary = 1)
-p <- as.matrix(c(0,7.4,1.8))
-q1 <- as.matrix(c(1,1/0.860716)) 
-q2 <- as.matrix(c(1,0.194642 + 0.614045 * j))
-q3 <- as.matrix(c(1,0.194642 - 0.614045 * j))
+p <- as.matrix(c(100/4,300/4))
+q1 <- as.matrix(c(1/8 + j * sqrt(15)/8,1))
+q2 <- as.matrix(c(1/8 - j * sqrt(15)/8,1))
+a <- matrix(0, 2)
+a
+p
+q1
+q2
+# p/(q1 q2) = A/q1 + B/q2
+# A q2 + B q1 = p
+m <- matrix(0, 2, 2)
+b <- matrix(0, 2)
+for (i in 1:2) {
+m[i,1] <- q2[i]
+m[i,2] <- q1[i]
+b[i] <- p[i]
+}
+#det(m)
+b <- solve(m) %*% b
+#b <- b * det(m)
+print(paste(b[1], b[2]))
+print(paste(b[1]/(1/8 + j * sqrt(15)/8), b[2]/(1/8 - j * sqrt(15)/8)))
+
+# p/q1/q2 = (a + (b[1] * r1 + b[2] * r2 + b[3] * r3)/q1/q2)
+p - (b[1] * q2 + b[2] * q1)
+polymul(q1, q2)
+
+###
+
+j <- complex(imaginary = 1)
+p <- as.matrix(c(-8,0,6))
+q1 <- as.matrix(c(- 2.93114246375354, 1))
+q2 <- as.matrix(c(0.46557123187677 + 1.58510398503090 * j, 1))
+q3 <- as.matrix(c(0.46557123187677 - 1.58510398503090 * j, 1))
 a <- matrix(0, 3)
 #a <- p/q1/q2/q3
 #p <- p %% (q1*q2*q3)
@@ -70,7 +100,7 @@ p
 q1
 q2
 q3
-# p/(q1 q2 q3) = A q1 + B q2 + C q3
+# p/(q1 q2 q3) = A/q1 + B/q2 + C/q3
 # A q2 q3 + B q1 q3 + C q1 q2 = p
 # A r1 + B r2 + C r3 = p
 r1 <- polymul(q2, q3)
@@ -87,10 +117,12 @@ b[i] <- p[i]
 #det(m)
 b <- solve(m) %*% b
 #b <- b * det(m)
-print(b)
+print(paste(b[1], b[2], b[3]))
+print(paste(b[1]/q1[1], b[2]/q2[1], b[3]/q3[1]))
+print(paste(1/q1[1], 1/q2[1], 1/q3[1]))
 
 p <- as.matrix(c(0,7.4,1.8))
-q1 <- as.matrix(c(1,1/0.860716)) 
+q1 <- as.matrix(c(1,1/0.860716))
 q2 <- as.matrix(c(1,0.194642 + 0.614045 * j))
 q3 <- as.matrix(c(1,0.194642 - 0.614045 * j))
 # p/q1/q2/q3 = (a + (b[1] * r1 + b[2] * r2 + b[3] * r3)/q1/q2/q3)
@@ -100,7 +132,7 @@ polysum(- polymul(polymul(polymul(q1, q2), q3), a), p - (b[1] * r1 + b[2] * r2 +
 
 x <- as.polynomial(c(0,1))
 p <- as.polynomial(c(0,1.8,7.4))
-q <- as.polynomial(c(2.8,3.5,2.1,1)) 
+q <- as.polynomial(c(2.8,3.5,2.1,1))
 p
 q
 for (i in 0:4) {
