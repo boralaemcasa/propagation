9d75834c1208bfabaea3c8dfe977163d96e6081e
unknown <boralaemcasa@gmail.com>
2024-07-16 03:56:15 -0300

    generality++

--- LaTeX/conics theorem/pg.py
+++ LaTeX/conics theorem/pg.py
@@ -1,52 +1,51 @@
 from mpmath import *
 import matplotlib.pyplot as plt
+
+def line(Xa, Ya, Xb, Yb, color):
+	t = mp.arange(0, 1, 1/500)
+	for i in range(0,500):
+		x[i] = Xa + t[i] * (Xb - Xa)
+		y[i] = Ya + t[i] * (Yb - Ya)
+	plt.plot(x, y, color)
+
+def intersect(X, Y, a, b, c, d):
+	#X[a] + t1 (X[b] - X[a]) = X[c] + t2 (X[d] - X[c])
+	M = mp.matrix([[X[b] - X[a], X[c] - X[d]], [Y[b] - Y[a], Y[c] - Y[d]]])
+	t = M**(-1) * mp.matrix([[X[c] - X[a]], [Y[c] - Y[a]]])
+	Ax = X[a] + t[0] * (X[b] - X[a])
+	Ay = Y[a] + t[0] * (Y[b] - Y[a])
+	line(X[a], Y[a], Ax, Ay, 'g')
+	line(X[c], Y[c], Ax, Ay, 'g')
+	line(X[a], Y[a], X[b], Y[b], 'b')
+	line(X[c], Y[c], X[d], Y[d], 'b')
+	return mp.matrix([[Ax], [Ay]])
+	
 mp.dps = 40
 p = mp.pi()
-t1 = mp.rand() * 2 * p #0
-t2 = mp.rand() * 2 * p #p/12
-t4 = mp.rand() * 2 * p #p/3*2
-t5 = mp.rand() * 2 * p #p/180*179
-cos_t1, sin_t1 = mp.cos(t1), mp.sin(t1)
-cos_t2, sin_t2 = mp.cos(t2), mp.sin(t2)
-cos_t4, sin_t4 = mp.cos(t4), mp.sin(t4)
-cos_t5, sin_t5 = mp.cos(t5), mp.sin(t5)
-L_15 = (-cos_t5 * sin_t1 + cos_t1 * sin_t5)/(cos_t1 - cos_t5)
-L_14 = (-cos_t4 * sin_t1 + cos_t1 * sin_t4)/(cos_t1 - cos_t4)
-L_24 = (-cos_t4 * sin_t2 + cos_t2 * sin_t4)/(cos_t2 - cos_t4)
-a = (sin_t2 - L_15)/cos_t2
-a2 = a*a
-for eps6 in list([-1,1]):
-	cos_t6 = (-a*L_15 + eps6 * mp.sqrt(1 + a2 - L_15*L_15))/(1 + a2)
-	sin_t6 = a * cos_t6 + L_15
-	c = (sin_t6 - L_14)/cos_t6
-	c2 = c*c
-	for eps3 in list([-1,1]):
-		cos_t3 = (-L_14*c + eps3 * mp.sqrt(1 + c2 - L_14*L_14))/(1 + c2)
-		sin_t3 = c*cos_t3 + L_14
-		L_35 = (-cos_t5 * sin_t3 + cos_t3 * sin_t5)/(cos_t3 - cos_t5)
-		M = mp.matrix([[1,-1],[-cos_t4,cos_t2]])
-		AB = 1/(cos_t2-cos_t4) * M * mp.matrix([[sin_t2],[sin_t4]])
-		M = mp.matrix([[1,-1],[-cos_t5,cos_t3]])
-		CD = 1/(cos_t3 - cos_t5) * M * mp.matrix([[sin_t3],[sin_t5]])
-		cx = (CD[1]-AB[1])/(AB[0]-CD[0])
-		cy = AB[0]*cx + AB[1]
-		if mp.fabs(cx) < 1e-9:
-			x = [cos_t4, 0,    cos_t1, 0,    cos_t5, 0,    cos_t3, cos_t6, cos_t2, 0,    cos_t4]
-			y = [sin_t4, L_14, sin_t1, L_15, sin_t5, L_35, sin_t3, sin_t6, sin_t2, L_24, sin_t4]
-			plt.plot(x, y, 'g')
-			print("x_C =", cx)
-			print("y_C =", cy)
-			x = [0, 0]
-			y = [min([L_15, L_14, L_35]), max([L_15, L_14, L_35])]
-			plt.plot(x, y, 'r')
-			x = mp.arange(0, 2*p, 2*p/500)
-			y = mp.arange(0, 2*p, 2*p/500)
-			for i in range(0,500):
-				x[i], y[i] = mp.cos(x[i]), mp.sin(x[i])
-			plt.plot(x, y, 'y')
-			plt.title("e_6 = " + str(eps6) + "; e_3 = " + str(eps3) + "; diff = " + str(L_35-L_24))
-			x = [cos_t4, cos_t1, cos_t5, cos_t3, cos_t6, cos_t2, cos_t4]
-			y = [sin_t4, sin_t1, sin_t5, sin_t3, sin_t6, sin_t2, sin_t4]
-			plt.plot(x, y, 'b')
-			plt.show()
-			exit()
+t = mp.zeros(6,3)
+for i in range(0,6):
+	t[i,0] = mp.rand() * 2 - 1#0
+	t[i,1] = mp.cosh(t[i,0])
+	t[i,2] = mp.sinh(t[i,0])
+x = mp.arange(-1, 1, 2/500)
+y = mp.arange(-1, 1, 2/500)
+for i in range(0,500):
+	x[i], y[i] = mp.cosh(x[i]), mp.sinh(x[i])
+plt.plot(x, y, 'y')
+#x, y = mp.zeros(7,1), mp.zeros(7,1)
+#x[:-1,0], y[:-1,0] = t[:,1], t[:,2]
+#x[6,0],   y[6,0]   = t[0,1], t[0,2]
+#plt.plot(x, y, 'b')
+A = intersect(t[:,1], t[:,2], 1-1, 5-1, 2-1, 6-1)
+B = intersect(t[:,1], t[:,2], 1-1, 4-1, 3-1, 6-1)
+C = intersect(t[:,1], t[:,2], 2-1, 4-1, 3-1, 5-1)
+line(A[0], A[1], B[0], B[1], 'r')
+line(B[0], B[1], C[0], C[1], 'r')
+Lambda1 = (C[0] - A[0])/(B[0] - A[0])
+Lambda2 = (C[1] - A[1])/(B[1] - A[1])
+print(Lambda2 - Lambda1)
+plt.show()
+# Release 0.1.1 from 2024/July/16
+# Vinicius Claudino Ferraz @ Santa Luzia, MG, Brazil
+# Out of charity, there is no salvation at all.
+# With charity, there is Evolution.
